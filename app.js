648ff76b353e0cb01873f0cd4a51e7bd92cc41dd
Leandro Souza <leandrofsbr@gmail.com>
2022-02-12 03:59:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+feather.replace()
+
+let socialMediaLinks = {
+    github: 'leandrofsbr',
+    youtube: 'watch?v=sTPXkVKwU24',
+    twitter: 'leandrofsbr',
+    instagram: 'leandrofs.design',
+    facebook: 'leandrofsbr',
+}
+
+function changeSocialMediaLinks(){
+    for(let li of socialLinks.children){
+        const social = li.getAttribute('class')
+        li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${socialMediaLinks.github}`
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userName.textContent = data.name
+        product.textContent = data.login
+        description.textContent = data.bio
+    })
+}
+
+getGitHubProfileInfos()
